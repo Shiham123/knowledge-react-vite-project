7443f26ab8423e6807d2820daf2af975863e74b4
Shiham123 <s36651786@gmail.com>
2023-09-13 10:21:38 +0600

    done adding blog page with out functionality

--- src/assets/components/blog.jsx
+++ src/assets/components/blog.jsx
@@ -1,3 +1,5 @@
+import { FaBookmark } from 'react-icons/fa';
+
 const BlogPage = (props) => {
   const { blog } = props;
   const {
@@ -13,6 +15,32 @@ const BlogPage = (props) => {
   return (
     <div className="mb-20 space-y-4">
       <img className="w-full mb-8" src={cover} alt={title} />
+      <div className="flex justify-between mb-4">
+        <div className="flex">
+          <img className="w-14" src={author_img} alt="" />
+          <div className="ml-16">
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} Min Read</span>
+          <button>
+            <FaBookmark />
+          </button>
+        </div>
+      </div>
+      <div className="text-4xl">{title}</div>
+      <p>
+        {hashtags.map((hash, index) => {
+          <span key={index}>
+            <a href="">{hash}</a>{' '}
+          </span>;
+        })}
+      </p>
+      <button className="text-purple-800 font-bold underline">
+        Mark as Read
+      </button>
     </div>
   );
 };
